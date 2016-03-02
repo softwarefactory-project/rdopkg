2ebf725edef293a87b6cf073fabb18bf4a7a8227
Jakub Ruzicka <jruzicka@redhat.com>
2016-03-02 17:41:28 +0100

    Add an example of rdoinfo frontend usage

---
+++ examples/rdo_projects.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+An example script that uses rdopkg.actionmods.rdoinfo to output a list of
+currently maintained RDO projects.
+"""
+
+from rdopkg.actionmods import rdoinfo
+
+
+def list_projects():
+    inforepo = rdoinfo.get_default_inforepo()
+    info = inforepo.get_info()
+    pkgs = info['packages']
+
+    pkg_filter = {
+        'name': '^openstack-'
+    }
+    pkgs = rdoinfo.filter_pkgs(pkgs, pkg_filter)
+
+    for pkg in pkgs:
+        project = pkg.get('project') or pkg['name']
+        print("### " + project)
+        print("package: " + pkg['name'])
+        print("code: " + pkg['upstream'])
+        print("maintainers: " + " ".join(pkg['maintainers']))
+        print("")
+
+
+if __name__ == '__main__':
+    list_projects()
