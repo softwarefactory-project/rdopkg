470cfef7bf479dda080b2ee99798d43e41c050ea
Jakub Ruzicka <jruzicka@redhat.com>
2016-08-09 16:33:07 +0200

    cbsbuild: make koji module optional
    Change-Id: Id290a2407151214c1a56c9728dc54d64d2a276b7

--- rdopkg/actionmods/cbsbuild.py
+++ rdopkg/actionmods/cbsbuild.py
@@ -13,6 +13,15 @@ from rdopkg.utils.specfile import spec_fn, Spec
 from rdopkg.utils.log import log
 from rdopkg import guess
 
+
+KOJI_AVAILABLE = False
+try:
+    import koji
+    KOJI_AVAILABLE = True
+except ImportError:
+    pass
+
+
 options = None
 
 
@@ -27,6 +36,8 @@ class KojiOpts(object):
 def new_build(profile='cbs', scratch=True):
     # Very ugly: some utilities are only available in koji CLI
     # and not in the koji module
+    if not KOJI_AVAILABLE:
+        raise exception.ModuleNotAvailable(module='koji')
     import imp
     kojibin = find_executable('koji')
     kojicli = imp.load_source('kojicli', kojibin)
