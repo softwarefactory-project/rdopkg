a7d4d4e671bea8d572ffd33ac85ea54deca78b22
Ihar Hrachyshka <ihrachys@redhat.com>
2016-10-03 03:10:55 +0000

    Fixed -c argument parsing
    Without the change, rdopkg -c will always print a help message. It
    should instead behave identically to --continue.
    
    This closes #85
    
    Change-Id: Ia2fe3f04051aa8f76b53b9da971ebe7f97b6a115

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -4,3 +4,13 @@ from rdopkg.cli import rdopkg
 def test_actions_availability():
     r = rdopkg('actions')
     assert r == 0, "Some action functions are NOT AVAILABLE"
+
+
+def test_actions_continue_short():
+    r = rdopkg('-c')
+    assert r == 1, "--continue succeeded with no state"
+
+
+def test_actions_continue_long():
+    r = rdopkg('--continue')
+    assert r == 1, "--continue succeeded with no state"
