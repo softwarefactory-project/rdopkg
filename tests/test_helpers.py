c894f9a8ad034beccf10d6279452f503ea7d0483
Jon Schlueter <jschluet@redhat.com>
2016-10-20 13:04:13 -0400

    Cleanup helpers.edit() Exception
    remove unused Exception variable and re-formatted Exception
    text to make it more readable.
    
    Added some Tests around this function to attempt to check
    msg from exception.  But missing pytest foo to get it
    asserted correctly
    
    Change-Id: Ie3f7738ba5ce04828545fd140e531b1aa13c595b

---
+++ tests/test_helpers.py
@@ -0,0 +1,33 @@
+from rdopkg import helpers
+from rdopkg import exception
+import pytest
+
+
+def test_editor_editor_run_failed(monkeypatch):
+
+    def mock_run(*args, **kwargs):
+        raise exception.CommandNotFound('Foo')
+    monkeypatch.setattr(helpers, 'run', mock_run)
+
+    def mock_env(var):
+        return 'foo_editor'
+    monkeypatch.setattr(helpers.os.environ, 'get', mock_env)
+
+    with pytest.raises(exception.CommandNotFound) as excinfo:
+        helpers.edit('/tmp/foo')
+    assert excinfo.match(r'.*\(foo_editor\).* Please set \$EDITOR .*')
+
+
+def test_editor_no_env(monkeypatch):
+
+    def mock_run(*args, **kwargs):
+        raise exception.CommandNotFound('Foo')
+    monkeypatch.setattr(helpers, 'run', mock_run)
+
+    def mock_env(var):
+        return ''
+    monkeypatch.setattr(helpers.os.environ, 'get', mock_env)
+
+    with pytest.raises(exception.CommandNotFound) as excinfo:
+        helpers.edit('/tmp/foo')
+    assert excinfo.match(r'.*\(vim\).* Please set \$EDITOR .*')
