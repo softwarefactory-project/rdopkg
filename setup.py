b2fc7e71729a4a8801b0d1babd7a93413beffb79
Javier Pena <jpena@redhat.com>
2016-10-20 10:01:07 +0200

    Add rdopkg.actions to setup.py
    It's missing in rdopkg 0.41, which breaks installs from PyPi.
    
    Change-Id: Ie4e07d42c5260ee292e1af9d6aae75911c6ab74f

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     author='Jakub Ruzicka',
     author_email='jruzicka@redhat.com',
     url='https://github.com/redhat-openstack/rdopkg',
-    packages=['rdopkg', 'rdopkg.utils', 'rdopkg.actionmods'],
+    packages=['rdopkg', 'rdopkg.utils', 'rdopkg.actionmods', 'rdopkg.actions'],
     install_requires=requires(),
     entry_points={
         "console_scripts": ["rdopkg = rdopkg.cli:main"]
