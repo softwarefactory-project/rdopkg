e3951c7e1ac1a90bfee7bd92f82012d11f6db3e9
David Moreau-Simard <dms@redhat.com>
2016-11-29 09:47:34 -0500

    Add findpkg integration tests to run_tests.sh
    These tests will try to ensure that we do not break functionality
    in findpkg with new patches.
    
    Co-Authored-By: Javier Pena <jpena@redhat.com>
    Change-Id: I8812220bc4e43cc326e8ad03b9207e00ab84b325

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,38 @@
 #!/bin/sh
 set -x
 
+GIT_BASE_URL="https://review.rdoproject.org/r/p"
+WORKSPACE="${WORKSPACE:-/tmp}"
+
 PYTHONPATH=. py.test $@
 pep8 rdopkg
+
+# Integration tests for findpkg
+function test_rdopkg_findpkg(){
+    PKG_NAME=$(rdopkg findpkg $1 -l ${WORKSPACE}/rdoinfo | awk '/^name/ {print $2}')
+    if [ $2 != $PKG_NAME ]; then
+        echo "$0 FAILED EXPECTED: $@ (GOT: $PKG_NAME)"
+        return 1
+    fi
+    return 0
+}
+
+if [ -e /usr/bin/zuul-cloner ]; then
+    zuul-cloner --workspace $WORKSPACE $GIT_BASE_URL rdoinfo
+else
+    # We're outside the gate, just do a regular git clone
+    pushd ${WORKSPACE}
+    # rm -rf first for idempotency
+    rm -rf rdoinfo
+    git clone "${GIT_BASE_URL}/rdoinfo" rdoinfo
+    popd
+fi
+
+test_rdopkg_findpkg glance                          openstack-glance
+test_rdopkg_findpkg glance-distgit                  openstack-glance
+test_rdopkg_findpkg openstack-glance                openstack-glance
+test_rdopkg_findpkg puppet-glance                   puppet-glance
+test_rdopkg_findpkg puppet/puppet-glance            puppet-glance
+test_rdopkg_findpkg glanceclient                    python-glanceclient
+test_rdopkg_findpkg openstack/glanceclient-distgit  python-glanceclient
+test_rdopkg_findpkg python-glanceclient             python-glanceclient
