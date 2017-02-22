7da0a117a8d4fb1c41be7d83ac9b310c7b3eee19
Jakub Ruzicka <jruzicka@redhat.com>
2017-02-22 19:44:34 +0100

    WIP: spec: new actions to parse/edit .spec files
    The new experimental spec actions module provides CLI interface for .spec file
    parsing/manipulation using rdopkg.utils.specfile module.
    
    This finally seems to work fine, need to cover with tests.
    
    TODO:
    * tests
    * docs
    
    Fixes: https://github.com/openstack-packages/rdopkg/issues/104
    Change-Id: I804c3ea96c2e7f6800aa938da95a4102b2e58e61

---
+++ rdopkg/actions/spec/actions.py
@@ -0,0 +1,31 @@
+from rdopkg.utils import specfile
+
+
+def get_tag(tag, expand_macros=False):
+    spec = specfile.Spec()
+    val = spec.get_tag(tag, expand_macros=expand_macros)
+    print(val)
+
+
+def set_tag(tag, value):
+    spec = specfile.Spec()
+    spec.set_tag(tag, value)
+    spec.save()
+
+
+def get_macro(macro, expand_macros=False):
+    spec = specfile.Spec()
+    val = spec.get_macro(macro, expanded=expand_macros)
+    print(val)
+
+
+def set_macro(macro, value):
+    spec = specfile.Spec()
+    spec.set_macro(macro, value)
+    spec.save()
+
+
+def spec_eval(expression):
+    spec = specfile.Spec()
+    val = spec.rpmspec.sourceHeader.format(expression)
+    print(val)
