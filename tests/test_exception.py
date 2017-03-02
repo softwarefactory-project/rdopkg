36eaaed4e1c7bf58aefc5b343746e9dab923883c
Jakub Ruzicka <jruzicka@redhat.com>
2017-03-02 16:30:20 +0100

    exception: support exit_code
    RdopkgException now contains exit_code which is returned by CLI on exit when
    exception is raised.
    
    Default exit_code can be set per exception and overriden when raising.
    
    Change-Id: I85ff74d1a99095a7e8a41e33c146512562e7598c

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -2,9 +2,10 @@ from rdopkg.utils.exception import CommandFailed
 from rdopkg.utils.exception import RdopkgException
 
 
-def test_exception_Default():
+def test_exception_default():
     e = RdopkgException()
     assert str(e) == 'An unknown error occurred'
+    assert e.exit_code == 1
 
 
 def test_exception_msg_provided():
@@ -20,3 +21,9 @@ def test_CommandFailed_bad_kwargs():
 def test_CommandFailed_good_kwargs():
     e = CommandFailed(cmd='bar')
     assert str(e) == e.msg_fmt % {'cmd': 'bar'}
+
+
+def test_exception_exit_code():
+    for code in [-128, -1, 0, 1, 23, 1337]:
+        e = RdopkgException(exit_code=code)
+        assert e.exit_code == code
