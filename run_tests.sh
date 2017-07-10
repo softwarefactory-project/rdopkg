f29452b949a8873fc1e43841f21fc480bb7a18e7
Sorin Sbarnea <ssbarnea@redhat.com>
2017-07-10 09:14:17 +0100

    Fixed text execution errors
    - remove precompiled files to avoid possible pytest failures
    - use of bash strict mode
    - install rdopkg and deps before testing and running it
    
    Change-Id: Ife806f1dec3bf0494058f2bf808b36d991189c81

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-set -ex
+set -exuo pipefail
 
+# avoid possible pytest errors due to precompiled files
+export PYTHONDONTWRITEBYTECODE=1
+find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf || true
+
+pip install -r test-requirements.txt -r requirements.txt -e .
 PYTHONPATH=. py.test $@
 pep8 rdopkg
 ./tests/test_findpkg_integration.sh
