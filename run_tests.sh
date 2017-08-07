6e486a128dacd88257b3cd72b1e95aba67c51faf
Test Runner <test@dev.null>
2017-08-07 17:25:31 +0200

    tests: use tox to setup and run tests
    tox is a standard python testing automation tool that allows setting up testing
    environments and testing under different versions of python in an uniform way.
    
    Running `tox` is now equivalent to old `run_tests.sh` on Python 2.7 and 3.5.
    
    run_tests.sh now simply invokes tox and might be removed in the future.
    
    Change-Id: I5e7d712215821f9d4c346f4392bef662ad781b65

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 set -exuo pipefail
 
-# avoid possible pytest errors due to precompiled files
-export PYTHONDONTWRITEBYTECODE=1
-find . | grep -E "(__pycache__|\.pyc|\.pyo)$" | xargs rm -rf || true
-
-PYTHONPATH=. py.test $@
-PYTHONPATH=. behave --format=progress
-python -m pycodestyle
-./tests/test_findpkg_integration.sh
+tox
