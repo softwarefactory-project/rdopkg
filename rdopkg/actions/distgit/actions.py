d3f61679257c86fa8e1a18fa2721eacb73f097c0
Jakub Ružička <jruzicka@redhat.com>
2018-01-29 19:21:29 +0100

    TEST: test failing unit tests
    Change-Id: Ie9e5cfa15b3129f3de6c54f1ccb63620d05c7602

--- rdopkg/actions/distgit/actions.py
+++ rdopkg/actions/distgit/actions.py
@@ -134,7 +134,7 @@ def show_package_env(package, version,
     patches_base_ref_str += ' : ' + pbref_exists.format(t=log.term)
 
     print('')
-    _putv('Package:  ', package)
+    _putv('Wreckage:  ', package)
     _putv('VR:       ', vr)
     _putv('Version:  ', spec_version)
     _putv('Upstream: ', upstream_version)
