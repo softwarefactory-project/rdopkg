3f6848fee0a77e3c14cc88d386f2fdcb76b6c420
Jakub Ruzicka <jruzicka@redhat.com>
2019-01-23 17:04:28 +0100

    WIP: integrate rpmlint & generic linting support
    It's working including unit tests, only few touches remain.
    
    TODO:
    * colors for better readability
    * replace old sanity checks system
    * docs (ACTION: lint ...)
    * SPLIT INTO MULTIPLE PATCHES:
    
      * rdopkg lint & generic linting refactor
      * integration into existing actions (replcaing sanity_check)
      * rpmlint support
    
    Change-Id: Ic5f48599d113a782e333e9ff013df27005c2d294

--- rdopkg/exception.py
+++ rdopkg/exception.py
@@ -65,19 +65,6 @@ class InvalidAction(RdopkgException):
     msg_fmt = "Invalid action: %(action)s"
 
 
-class BuildArchSanityCheckFailed(RdopkgException):
-    msg_fmt = ("Due to mysterious ways of rpm, BuildArch needs to be placed "
-               "AFTER SourceX and PatchX lines in .spec file, "
-               "otherwise %%{patches} macro will be empty "
-               "and both %%autosetup and `git am %%{patches}` will fail.\n\n"
-               "Please move BuildArch AFTER SourceX and PatchX.")
-
-
-class DuplicatePatchesBaseError(RdopkgException):
-    msg_fmt = ("Please make sure to only have one "
-               "# patches_base= entry in .spec")
-
-
 class InvalidGitRef(RdopkgException):
     msg_fmt = "Invalid git reference: %(ref)s"
 
@@ -211,3 +198,12 @@ class NoPatchesChanged(RdopkgException):
 
 class NoDistgitChangesFound(RdopkgException):
     msg_fmt = "No distgit changes found"
+
+
+class InvalidLintCheck(RdopkgException):
+    msg_fmt = "Invalid lint check: %(check)s"
+
+
+class LintProblemsFound(RdopkgException):
+    exit_code = 23
+    msg_fmt = "Lint problems detected."
