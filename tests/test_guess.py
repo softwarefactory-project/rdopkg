aaf330393cb91cb09ef0b0ae408710946830e232
Jon Schlueter <jschluet@redhat.com>
2019-03-07 13:29:26 -0500

    WIP: expand test_guess unit test coverage for patches branch
    Change-Id: Ibbe13aeba4da8dd1ecd9dfe8d78cbc2cfd0e806c

--- tests/test_guess.py
+++ tests/test_guess.py
@@ -1,6 +1,8 @@
 from rdopkg import guess
 from collections import namedtuple
 import pytest
+from unittest.mock import patch
+from unittest.mock import MagicMock
 
 VersionTestCase = namedtuple('VersionTestCase', ('expected', 'input_data'))
 
@@ -56,3 +58,52 @@ def test_version2tag_type1():
 
 def test_version2tag_type2():
     assert 'V1.2.3' == guess.version2tag('1.2.3', 'VX.Y.Z')
+
+
+find_patches_data = [
+        ('rhos-15.0-rhel-8', ['rhos-15.0-patches'], 'rhos-15.0-patches'),
+        ('foo-bar', ['foo-patches'], 'foo-patches'),
+        ('foo-bar', ['foo-bar-patches'], 'foo-bar-patches'),
+        ('foo-bar', ['refs/remotes/patches/foo-bar-patches'], 'foo-bar-patches'),
+        ('foo-bar', [''], None),
+
+        ('rhos-15.0-rhel-8-trunk', ['rhos-15.0-trunk-patches'], 'rhos-15.0-trunk-patches'),
+    ]
+
+
+@patch('rdopkg.guess.git')
+def test_find_patches_branch(git_mock):
+    git_mock.config_get = MagicMock(return_value='')
+    def ref_exists(path):
+        data = ['rhos-15.0-patches', 'refs/remotes/patches/rhos-15.0-patches']
+        return path in data
+    git_mock.ref_exists = ref_exists
+
+    # TODO Mock git.config_get('rdopkg.<distgit>.patches-branch')
+    # TODO Mock git.ref_exists(<path>) to check paths in list
+    expected = 'patches/rhos-15.0-patches'
+    remote = 'patches'
+    distgit = 'rhos-15.0-rhel-8'
+    assert expected == guess.find_patches_branch(distgit, remote)
+
+@patch('rdopkg.guess.git')
+def test_find_patches_branch_2(git_mock):
+    git_mock.config_get = MagicMock(return_value='')
+    def ref_exists(path):
+        data = [
+                'rhos-15.0-trunk-patches',
+                'refs/remotes/patches/rhos-15.0-trunk-patches',
+                'rhos-15.0-rhel-8-trunk-patches',
+                'refs/remotes/patches/rhos-15.0-rhel-8-trunk-patches'
+                'rhos-15.0-patches',
+                'refs/remotes/patches/rhos-15.0-patches'
+            ]
+        return path in data
+    git_mock.ref_exists = ref_exists
+
+    # TODO Mock git.config_get('rdopkg.<distgit>.patches-branch')
+    # TODO Mock git.ref_exists(<path>) to check paths in list
+    expected = 'patches/rhos-15.0-trunk-patches'
+    remote = 'patches'
+    distgit = 'rhos-15.0-rhel-8-trunk'
+    assert expected == guess.find_patches_branch(distgit, remote)
