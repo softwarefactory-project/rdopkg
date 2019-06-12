3fe63b1847f74cfe0625f4b7b263a1aacabafc64
Tristan Cacqueray <tdecacqu@redhat.com>
2019-06-12 05:55:39 +0000

    ci: use homedirectory for workspace
    This should fix recent ci flakyness.
    
    Change-Id: Iab0363da34d5beff567a41daa0e23cda3e72796f

--- tests/integration/test_integration.sh
+++ tests/integration/test_integration.sh
@@ -3,6 +3,12 @@
 
 set -e
 
+env
+if [ "${USER}" == "zuul-worker" ]; then
+    export WORKSPACE=/home/zuul-worker
+    echo "set workspace to $WORKSPACE"
+fi
+
 TESTS_PATH="`dirname \"$0\"`"
 TESTS_PATH="`( cd \"$TESTS_PATH\" && pwd )`"
 
