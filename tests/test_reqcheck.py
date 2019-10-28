175d41df540c28850f0905148219f1fc2bc08080
Joel Capitao <jcapitao@redhat.com>
2019-10-28 11:49:45 +0100

    Handle blacklisted version in requirements during reqcheck
    When a package meets the requirements but have some blacklisted
    versions, reqcheck output the package as MISMATCH.
    This change ignores the blacklisted versions during the comparison.
    
    Unit and integration test file for reqcheck have been added as well.
    
    Additionally, we add the option -R for reqcheck which gives the
    possibility to specify from which version git ref we get the
    requirements.txt. This is necessary to be able to make tests which
    have to be time independent (not chasing upstream master in
    that case).
    
    Change-Id: I078a2fbf6aebde3638a686db5333da91f18d7831

---
+++ tests/test_reqcheck.py
@@ -0,0 +1,56 @@
+import pytest
+import subprocess
+
+from rdopkg.cli import rdopkg
+from rdopkg.actionmods.reqs import CheckReq
+
+import test_common as common
+
+
+def _assert_sanity_out(o):
+    assert 'None' not in o
+
+
+def test_reqcheck(tmpdir, capsys):
+    dist_path = common.prep_spec_test(tmpdir, 'reqcheck')
+    with dist_path.as_cwd():
+        rv = rdopkg('reqcheck', '-R', 'master')
+    cap = capsys.readouterr()
+    o = cap.out
+    _assert_sanity_out(o)
+    assert 'MISSING:' not in o
+
+
+def test_checkreq_01():
+    cr = CheckReq('mypackage', '>= 1.2.3,!= 1.2.5', '>= 1.2.3')
+    got = cr.met()
+    expected = True
+    assert got == expected
+
+
+def test_checkreq_02():
+    cr = CheckReq('mypackage', '>= 1.2.3', '> 1.2.3')
+    got = cr.met()
+    expected = False
+    assert got == expected
+
+
+def test_checkreq_03():
+    cr = CheckReq('mypackage', '>= 1.2.3', '')
+    got = cr.met()
+    expected = False
+    assert got == expected
+
+
+def test_checkreq_04():
+    cr = CheckReq('mypackage', '', '>= 1.2.3')
+    got = cr.met()
+    expected = False
+    assert got == expected
+
+
+def test_checkreq_05():
+    cr = CheckReq('mypackage', '', '')
+    got = cr.met()
+    expected = True
+    assert got == expected
